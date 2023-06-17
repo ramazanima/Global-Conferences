edf5133c63010749af519519427ce57bdbaf9f19
Ali Ramazani <ramazanim@berea.edu>
2023-06-17 12:08:01 -0400

    added sessions router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const data = require("./src/data/sessions.json");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,23 +15,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
-sessionsRouter.route("/") // Corrected "Route" to "route"
-    .get((req, res) => {
-        res.send("session started");
-    });
+sessionsRouter.route("/")
+  .get((req, res) => {
+    res.render("sessions", { sessions: data }); // Pass the 'sessions' data to the template
+  });
 
-sessionsRouter.route("/1") // Corrected "Route" to "route"
-    .get((req, res) => {
-        res.send("single session");
-    });
+sessionsRouter.route("/1")
+  .get((req, res) => {
+    res.send("single session");
+  });
 
-app.use("/sessions", sessionsRouter); // Corrected "sessionRouter" to "sessionsRouter"
+app.use("/sessions", sessionsRouter);
 
 app.get("/", (req, res) => {
-    res.render("index", { title: "Web app", data: ["Bio", "Chem", "Hydro"], names: ["Ali", "Gagan", "Bishal"] });
+  res.render("index", { title: "Web app", data: ["Bio", "Chem", "Hydro"], names: ["Ali", "Gagan", "Bishal"] });
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    debug("Debugging mode");
+  console.log(`Listening on port ${PORT}`);
+  debug("Debugging mode");
 });
