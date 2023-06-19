cb955ae241132d80cd931b414b295536adc52a1d
Ali Ramazani <ramazanim@berea.edu>
2023-06-19 15:13:01 -0400

    debugged sesssionsRouter

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -7,24 +7,24 @@ const data = require("../data/sessions.json");
 const adminRouter = express.Router();
 
 adminRouter.route("/").get(async (req, res) => {
-  const url = "mongodb+srv://ramazanim:NLTuPDLq0v8kLT3G@globomantrics.ig51dfh.mongodb.net/?retryWrites=true&w=majority";
-  const dbName = "globomantics";
+//   const url = "mongodb+srv://ramazanim:NLTuPDLq0v8kLT3G@globomantrics.ig51dfh.mongodb.net/?retryWrites=true&w=majority";
+//   const dbName = "globomantics";
 
-  try {
-    const client = await MongoClient.connect(url);
-    debug("Connected to MongoDB");
+//   try {
+//     const client = await MongoClient.connect(url);
+//     debug("Connected to MongoDB");
 
-    const db = client.db(dbName);
+//     const db = client.db(dbName);
 
-    const response = await db.collection("data").insertMany(data);
+//     const response = await db.collection("data").insertMany(data);
 
-    debug("Data inserted into the database:", response);
+//     debug("Data inserted into the database:", response);
 
-    res.json(response);
-  } catch (error) {
-    debug("Error connecting to MongoDB:", error);
-    res.status(500).json({ error: "Failed to connect to the database" });
-  }
+//     res.json(response);
+//   } catch (error) {
+//     debug("Error connecting to MongoDB:", error);
+//     res.status(500).json({ error: "Failed to connect to the database" });
+//   }
 });
 
 module.exports = adminRouter;
--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,19 +1,54 @@
 const express = require("express");
 const data = require("../data/sessions.json");
 
+const debug = require("debug")("app:sessionsRouter");
+const { MongoClient, ObjectId } = require("mongodb");
+
 const sessionsRouter = express.Router();
 
+sessionsRouter.route("/").get(async (req, res) => {
+  const url =
+    "mongodb+srv://ramazanim:NLTuPDLq0v8kLT3G@globomantrics.ig51dfh.mongodb.net/?retryWrites=true&w=majority";
+  const dbName = "globomantics";
 
-sessionsRouter.route("/").get((req, res) => {
-    res.render("sessions", 
-    { sessions: data }); // Pass the 'sessions' data to the template
-  });
+  try {
+    const client = await MongoClient.connect(url);
+    debug("Connected to MongoDB");
 
-sessionsRouter.route("/:id").get((req, res) => {
-    const id = req.params.id;
+    const db = client.db(dbName);
+    const sessions = await db.collection("data").find().toArray();
+
+    debug("Data fetched from the database:", sessions);
+
+    res.render("sessions", { sessions });
+  } catch (error) {
+    debug("Error connecting to MongoDB:", error);
+    res.status(500).json({ error: "Failed to connect to the database" });
+  }
+});
+
+sessionsRouter.route("/:id").get(async (req, res) => {
+  const id = req.params.id;
+  const url =
+    "mongodb+srv://ramazanim:NLTuPDLq0v8kLT3G@globomantrics.ig51dfh.mongodb.net/?retryWrites=true&w=majority";
+  const dbName = "globomantics";
+
+  try {
+    const client = await MongoClient.connect(url);
+    debug("Connected to MongoDB");
+
+    const db = client.db(dbName);
+    const session = await db.collection("data").findOne({ _id: new ObjectId(id) });
+
+    debug("Data fetched from the database:", session);
+
     res.render("session", {
-        session: data[id],
+      session: session,
     });
-  });
+  } catch (error) {
+    debug("Error connecting to MongoDB:", error);
+    res.status(500).json({ error: "Failed to connect to the database" });
+  }
+});
 
-  module.exports = sessionsRouter;
+module.exports = sessionsRouter;
